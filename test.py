40dc8d111cf3d9bad2b3ba37d0ec62223377ff8a
Stefano <81710193+StefanoB2018@users.noreply.github.com>
2023-05-28 01:51:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,53 +9,47 @@ URL = 'https://discord.com/api/webhooks/1104425166366318724/sNM2A81tQdaboLkFlDvR
 keys = ['allPlayerList','winPlayerList','mapName','playerData','replayName']
 beforeKeys = ["**Players:**\n","**Winners:**\n","**Chosen Map:**\n","**Player Stats:**\n","\n**Replay Name:**\n"]
 count = 0
-while True:
-    try: 
-        r = get('http://localhost:5000/api/get/event/GameOver')
-        count += 1
-        print("Get #" + str(count))
-        response = r.text
-        data = json.loads(response)
-        s = ""
-        data = json.loads(data)
-        print(data, type(data))
-        matchData = data['gameOverData']
-        matchTime = str(data['gameTime'])
-        replay = matchData['replayName']
-        nomemappa = matchData['mapName']
-        nomemappa = nomemappa.replace('_',' ')
-        path = os.path.join(os.getcwd()+'/rw-hps/StartServer/data/replays/',replay)
-        
-        for i in range(len(keys)):
-            if(i != 3):
-                s += beforeKeys[i] 
-                temp = matchData[keys[i]]
-                if(i == 2 or i == 4):
-                        s += '`' 
-                for t in temp:
-                    if(i != 2 and i != 4):
-                        s += '`' 
-                    s += str(t)
-                    if(i != 2 and i != 4):
-                        s += "`\n"
-                if(i == 2 or i == 4):
-                        s += '`' 
-        s += '\n**Game time:**\n`'+ matchTime + 's`'
-        print(s)
-        payout ={
-            'content': s
-        }
-        #req.post(URL,data=payout)
-        time.sleep(10)
-        webhook = DiscordWebhook(url=URL)
-        
-        f = open(path, "rb")
-        embed = DiscordEmbed(title= nomemappa, description=s, color='03b2f8')
-        webhook.add_embed(embed)
-        webhook.add_file(file=f.read(), filename=replay)
-        response = webhook.execute(remove_embeds=True, remove_files= True)
-        f.close()
-        time.sleep(2)
-    except Exception as e:
-        print("Empty")
-        time.sleep(5)
+r = get('http://localhost:5000/api/get/event/GameOver')
+count += 1
+print("Get #" + str(count))
+response = r.text
+data = json.loads(response)
+s = ""
+data = json.loads(data)
+print(data, type(data))
+matchData = data['gameOverData']
+matchTime = str(data['gameTime'])
+replay = matchData['replayName']
+nomemappa = matchData['mapName']
+nomemappa = nomemappa.replace('_',' ')
+path = os.path.join(os.getcwd()+'/rw-hps/StartServer/data/replays/',replay)
+
+for i in range(len(keys)):
+    if(i != 3):
+        s += beforeKeys[i] 
+        temp = matchData[keys[i]]
+        if(i == 2 or i == 4):
+                s += '`' 
+        for t in temp:
+            if(i != 2 and i != 4):
+                s += '`' 
+            s += str(t)
+            if(i != 2 and i != 4):
+                s += "`\n"
+        if(i == 2 or i == 4):
+                s += '`' 
+s += '\n**Game time:**\n`'+ matchTime + 's`'
+print(s)
+payout ={
+    'content': s
+}
+#req.post(URL,data=payout)
+time.sleep(10)
+webhook = DiscordWebhook(url=URL)
+
+f = open(path, "rb")
+embed = DiscordEmbed(title= nomemappa, description=s, color='03b2f8')
+webhook.add_embed(embed)
+webhook.add_file(file=f.read(), filename=replay)
+response = webhook.execute(remove_embeds=True, remove_files= True)
+f.close()
